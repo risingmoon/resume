bf1b1d757415fc4eaa0fd09516839fd0b0375269
Justin Lee <justindavidlee88@gmail.com>
2016-07-24 01:02:07 -0700

    Set index.html

--- js/resume.js
+++ js/resume.js
@@ -1,10 +1,55 @@
-var HeaderModel = Backbone.Model.extend({
+/*Resume
+ */
+var app = app || {};
+
+app.HeaderModel = Backbone.Model.extend({
   url: "data/resume.json"
 });
+
+
+app.EducationCollection = Backbone.Collection.extend({
+  section: "#education",
+  model: Backbone.Model,
+  url: 'data/education.json'
+});
+
+app.EducationView = Backbone.View.extend({
+  template: _.template("<h2>Education</h2>"),
+  initialize: function() {
+	this.model.bind('change', this.render, this);
+	this.model.fetch();
+  },
+  render: function() {
+    return this.template(this.model.attributes);
+  }
+});
+
+app.SectionView = Backbone.View.extend({
+  el: 'body',
+  template: _.template($("#education").html()),
+  initialize: function(){
+	this.collection.fetch({reset: true})
+	this.render();
+
+    //this.listenTo( this.collection, 'add', this.renderEducation );
+	//this.listenTo( this.collection, 'reset', this.render );
+	//this.model.bind('change', this.render, this);
+	//this.model.fetch();
+  },
+  render: function() {
+	this.collection.each(function( item ) {
+	  this.renderModel( item );
+	}, this );
+    //this.$el.append(this.template(this.model.attributes));
+	//return this;
+  }
+});
+
+
 var HeaderView = Backbone.View.extend({
   el: 'body',
   template: _.template($('#header').html()),
-  model: new HeaderModel,
+  model: new app.HeaderModel,
   initialize: function(){
 	this.model.bind('change', this.render, this);
 	this.model.fetch();
@@ -16,15 +61,6 @@ var HeaderView = Backbone.View.extend({
 	return this;
   }
 });
-var ResumeView = Backbone.View.extend({
-  el: 'body',
-  initialize: function(){
-    this.render();
-  },
-  render: function(){
-    this.$el.html("<h1>TEST</h1>");
-  }
-});
 
-//var resumeView = new ResumeView();
-var headerView = new HeaderView();
+//new HeaderView();
+
